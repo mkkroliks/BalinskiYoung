01c545f485b0af30b3ac5d4fa9c2046afe3b3130
mkkroliks <maciej.krolikowski@appunite.com>
2016-03-23 11:58:40 +0100

    Initial Commit

---
+++ BalinskiIYoung/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BalinskiIYoung
+//
+//  Created by Maciej Królikowski on 23/03/16.
+//  Copyright © 2016 Maciej Królikowski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BalinskiIYoung/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BalinskiIYoung
+//
+//  Created by Maciej Królikowski on 23/03/16.
+//  Copyright © 2016 Maciej Królikowski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
